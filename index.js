5cfc75607c963e7dee4cadcab2b6c19d4569850f
DESKTOP-A20E83D\minseok <su960618@gmail.com>
2020-06-23 12:58:18 +0900

    2020.6.23 Nomad Coder React로 영화 웹 서비스 만들기 기초 코딩

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
-import Potato from "./Potato";
+// import Potato from "./Potato";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Potato />,
+    {/* <Potato /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
